f509ec83e6e3e6612e017402dd6576b8386e2412
André misuraca <m1sur4k@gmail.com>
2023-05-06 01:55:52 -0300

    criacao das services do orquestrador

---
+++ OnTheFly/Services/PostOfficesService.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using OnTheFly.Models;
+
+namespace OnTheFlyApp.Services
+{
+    public class PostOfficesService
+    {
+        static readonly HttpClient address = new HttpClient();
+        public async Task<Address> GetAddress(string cep)
+        {
+            try
+            {
+                HttpResponseMessage response = await PostOfficesService.address.GetAsync("https://viacep.com.br/ws/" + cep + "/json/");
+                response.EnsureSuccessStatusCode();
+                string ender = await response.Content.ReadAsStringAsync();
+                var end = JsonConvert.DeserializeObject<Address>(ender);
+                return end;
+            }
+            catch (HttpRequestException)
+            {
+                return null;
+            }
+        }
+    }
+}
