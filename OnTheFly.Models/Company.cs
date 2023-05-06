c95b3a9056eaa7802c1cf985694727422e654e61
Vinicius Diniz <viniciusdiniz.ti/2gmail.com>
2023-05-06 19:55:57 -0300

    update company e appsettings

--- OnTheFly.Models/Company.cs
+++ OnTheFly.Models/Company.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,11 @@ namespace OnTheFly.Models
     {
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+        [StringLength(14)]
         public string Cnpj { get; set; }
-        public string Name { get; set; }
+        [StringLength(30)]
+        public string? Name { get; set; }
+        [StringLength(30)]
         public string NameOpt { get; set; }
         public DateTime DtOpen { get; set; }
         public bool? Status { get; set; }
