9b84cec59507eb05bcbf97bd95c9f375ac2012da
yuri-miotto <yurimagrinimiotto@gmail.com>
2023-05-06 22:31:39 -0300

    iniciado as operacoes com a orquestradora consumindo o AirCraftService

---
+++ OnTheFly/Services/AirCraftService.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using OnTheFly.Models;
+
+namespace OnTheFlyApp.Services
+{
+    public class AirCraftService
+    {
+        static readonly HttpClient aircraftClient = new HttpClient();
+        static readonly string endpoint = "https://localhost:7195/api/PassengersService";
+
+        public async Task<AirCraft> Insert(AirCraft aircraft)
+        {
+            return new();
+        }
+
+        public async Task<List<AirCraft>> FindAll()
+        {
+            try
+            {
+                HttpResponseMessage response = await AirCraftService.aircraftClient.GetAsync(endpoint);
+                response.EnsureSuccessStatusCode();
+                string aircraftJson = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<AirCraft>>(aircraftJson);
+            }
+            catch (HttpRequestException e)
+            {
+                throw;
+            }
+        }
+
+        public async Task<AirCraft> FindByRab(string rab)
+        {
+            return new();
+        }
+
+        public async Task<AirCraft> Update(string rab, DateTime dtLastFlight)
+        {
+            return new();
+        }
+
+        public async Task Delete(string rab)
+        {
+
+        }
+    }
+}
